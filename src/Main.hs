59877c9f0c042d9d9487479d2c6fd494f3737cbc
zypeh <zypeh.geek@gmail.com>
2020-08-26 14:59:59 +0800

    randomVal gen function and task 1

--- src/Main.hs
+++ src/Main.hs
@@ -3,17 +3,22 @@ module Main where
 import System.Random
 import System.ProgressBar
 import Control.Concurrent (threadDelay)
-import Data.Foldable (for_)
+import Control.Monad
 import qualified System.Console.Pretty as P
+import Data.List (genericLength)
 
 -- https://www.wikiwand.com/en/Beta_distribution
 data BetaDist = BetaDist { alpha :: Int, beta :: Int } deriving (Show)
 data Color = Yellow | Red | Blue deriving (Show)
-data Customer = Customer Color BetaDist deriving (Show)
+data Customer = Customer { color :: Color, betaDist :: BetaDist } deriving (Show)
 
-yellowCustomer = Customer Yellow BetaDist { alpha = 2, beta = 5 }
-redCustomer = Customer Red BetaDist { alpha = 2, beta = 2 }
-blueCustomer = Customer Blue BetaDist { alpha = 5, beta = 1 }
+yellowCustomer = Customer {
+  color = Yellow,
+  betaDist = BetaDist { alpha = 2, beta = 5 }
+}
+
+-- redCustomer = Customer Red BetaDist { alpha = 2, beta = 2 }
+-- blueCustomer = Customer Blue BetaDist { alpha = 5, beta = 1 }
 
 {- 
     
@@ -33,6 +38,9 @@ gen_probability :: RandomGen g => Double -> g -> Double
 gen_probability alpha g = -alpha * log (1 - u)
   where (u, _) = random g
 
+gen_probability' :: Double -> Double -> Double
+gen_probability' alpha u = -alpha * log (1 - u)
+
 probability_density_fn :: Double -> BetaDist -> Double
 probability_density_fn x beta_dist = rho * x ^ (a - 1) * (1 - x) ^ (b - 1)
   where
@@ -40,21 +48,32 @@ probability_density_fn x beta_dist = rho * x ^ (a - 1) * (1 - x) ^ (b - 1)
     a = alpha beta_dist
     b = beta beta_dist
 
-work :: IO ()
-work = threadDelay 1000000 -- 1 second
-
-toBeDone :: Int -> [()]
-toBeDone n = replicate n ()
+average :: (Real a, Fractional b) => [a] -> b
+average xs = realToFrac (sum xs) / genericLength xs
 
 main = do
-  let iterateTime = 100
-  
+  let iterateTime = 10000000
   putStrLn $ P.color P.Yellow "Task 1"
-  pb <- newProgressBar defStyle 10 (Progress 0 iterateTime ())
 
-  for_ (toBeDone iterateTime) $ \() -> do
-    work
+  pb <- newProgressBar defStyle 10 (Progress 0 iterateTime ())
+  res <- replicateM iterateTime $ do
     incProgress pb 1
+    let alpha_yellow = (alpha . betaDist) yellowCustomer
+    randomVal <- randomIO :: IO Double
+    let x = gen_probability' (fromIntegral alpha_yellow) randomVal
+    pure $ probability_density_fn x (betaDist yellowCustomer)
   
-  putStrLn "Given only red customers, what are the average and maximum queue lengths in-front of the teller?"
-  putStrLn "Which type of customer(yellow, red or blue) gives the cloest value between the averate and maximum customer waiting times?"
+  putStrLn . P.color P.Yellow $ "Average waiting time: " <> show (average res) <> "second(s)."
+  putStrLn . P.color P.Yellow $ "Maximum waiting time: " <> show (maximum res) <> "second(s)."
+  putStrLn ""
+
+  putStrLn $ P.color P.Red "Task 2"
+  updateProgress pb (const $ Progress 0 iterateTime ())
+  replicateM_ iterateTime $ do
+    incProgress pb 1
+    let alpha_yellow = (alpha . betaDist) yellowCustomer
+    randomVal <- randomIO :: IO Double
+    let x = gen_probability' (fromIntegral alpha_yellow) randomVal
+    pure $ probability_density_fn x (betaDist yellowCustomer)
+
+  -- putStrLn "Which type of customer(yellow, red or blue) gives the cloest value between the averate and maximum customer waiting times?"
